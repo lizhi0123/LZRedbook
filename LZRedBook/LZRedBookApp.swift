858d345e21e4b2faed6c107396241f975319edc6
LiZhi <zhang_zhi_li@163.com>
2024-06-05 16:35:24 +0800

    添加项目

---
+++ LZRedBook/LZRedBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  LZRedBookApp.swift
+//  LZRedBook
+//
+//  Created by LiZhi on 2024/6/5.
+//
+
+import SwiftUI
+
+@main
+struct LZRedBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
